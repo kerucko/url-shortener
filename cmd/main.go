944188d9f653c8fd69edc51ac494da9a31eda5ee
kerucko <cykinpavel@gmail.com>
2024-01-23 03:40:04 +0300

    config

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"go_url_shortener/internal/config"
+	"log"
+	"net/http"
+
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	_ = chi.NewRouter()
+	cfg := config.MustLoad()
+
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	log.Fatal(http.ListenAndServe(":"+cfg.Server.Port, router))
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,41 @@
 package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Server `yaml:"server"`
+	Redis  `yaml:"redis"`
+}
+
+type Server struct {
+	Port string `yaml:"port"`
+}
+
+type Redis struct {
+	Host    string        `yaml:"host"`
+	Port    string        `yaml:"port"`
+	Timeout time.Duration `yaml:"timeout"`
+}
+
+func MustLoad() Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("config path is not set")
+	}
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist: %s", configPath)
+	}
+
+	var config Config
+	err := cleanenv.ReadConfig(configPath, &config)
+	if err != nil {
+		log.Fatalf("config not read: %v", err)
+	}
+	return config
+}
